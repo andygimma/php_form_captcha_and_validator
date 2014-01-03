d76f5ce5870452e8aec81b8eb3dbebc5aae9d61d
andygimma <andy.n.gimma@gmail.com>
2014-01-03 06:22:15 -0500

    checking if local server

--- contactus.php
+++ contactus.php
@@ -6,10 +6,9 @@ function rpHash($value) {
         $hash = (($hash << 5) + $hash) + ord(substr($value, $i)); 
     } 
     return $hash; 
-} 
+}
 
 if (rpHash($_POST['defaultReal']) == $_POST['defaultRealHash']) { 
-//   echo "passed test";
   $fullname = $_POST['fullname'];
   $email = "stiehls@stiehlsbodymod.c	om";
   $subject = $_POST['subject'];
@@ -17,8 +16,12 @@ if (rpHash($_POST['defaultReal']) == $_POST['defaultRealHash']) {
   $formcontent = "From: $fullname \n Message: $message";
   $recipient = "zachary.a.straub@gmail.com";
   $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-  echo "<center><br><br><br>Thank You! Click <a href=\"index.html\">here to return</a> to the Home Page"; 
+  if ('127.0.0.1' == $_SERVER["REMOTE_ADDR"]) {
+    echo "Success!";
+  } else {
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "<center><br><br><br>Thank You! Click <a href=\"index.html\">here to return</a> to the Home Page"; 
+  }
 } else {
   echo "<center><br><br><br>Validation entered incorrectly"; 
 }
